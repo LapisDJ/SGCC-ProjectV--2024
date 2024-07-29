0183176de1c717470519e558de5166337371d47b
Rth2608 <utaehun2608@naver.com>
2024-07-29 23:38:09 +0900

    feat : 샷건 , 권총 , 바주카포 구현완료 ( PlayerController.cs : 권총 바주카포 회전해서 몬스터 유도 , 바주카포 폭발이펙트 반지름에 따라 구현되도록 )

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -4,17 +4,28 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Vector2 inputVec;        // 방향키 좌표값을 입력받을 변수
+    private Vector2 lastDirection;  // 플레이어가 마지막으로 바라본 방향 저장
+    
     PlayerStat playerStat;
     [SerializeField]
     GameObject HPBar;
-
     [SerializeField]
     GameObject Bullet;
     float BulletSpeed = 10.0f;
 
     [SerializeField]
-    float shootCooldown = 1.0f; // 총알 발사 쿨타임
+    GameObject Rocket;
+    float RocketSpeed = 10.0f;
+
+    [SerializeField]
+    float shootCooldown = 3.0f; // 권총 발사 쿨타임
+    float shootGunCooldown = 5.0f;  //샷건 발사 쿨타임
+    float bazookaCooldown = 5.0f; // 바주카포 발사 쿨타임
     private float lastShootTime;
+    private float lastBazookaTime;
+
+    private bool isBazookaAutoFire = false; // 바주카포 On Off로 구현
 
     Rigidbody2D rb;
 
@@ -36,6 +47,10 @@ public class PlayerController : MonoBehaviour
         {
             Debug.LogError("Rigidbody2D is missing on the Player.");
         }
+        else
+        {
+            rb.constraints = RigidbodyConstraints2D.FreezeRotation; // Rigidbody의 회전을 고정
+        }
 
         if (HPBar == null)
         {
@@ -49,11 +64,21 @@ public class PlayerController : MonoBehaviour
         }
 
         lastShootTime = -shootCooldown; // 게임 시작 시 즉시 발사 가능하도록 설정
+        lastBazookaTime = -bazookaCooldown;
         lastMeleeAttackTime = -meleeAttackCooldown; // 게임 시작 시 즉시 근접 공격 가능하도록 설정
+
     }
 
     void Update()
     {
+        // 매 프레임당 방향키 입력받기
+        inputVec.x = Input.GetAxisRaw("Horizontal");   // 좌측방향키는 -값 , 우측방향키는 +값으로 inputVec.x에 업데이트
+        inputVec.y = Input.GetAxisRaw("Vertical");     // 아래방향키는 -값 , 윗방향키는 +값으로 inputVec.y에 업데이트 
+        if (inputVec != Vector2.zero)
+        {
+            lastDirection = inputVec.normalized;
+        }
+
         if (Input.GetKey(KeyCode.X) && Time.time >= lastShootTime + shootCooldown)
         {
             ShootBullet();
@@ -65,27 +90,31 @@ public class PlayerController : MonoBehaviour
             MeleeAttack();
             lastMeleeAttackTime = Time.time; // 마지막 근접 공격 시간 업데이트
         }
+
+
+
+        if (Input.GetKey(KeyCode.B) && Time.time >= lastShootTime + shootCooldown)  // B키 권총
+        {
+            ShootBullet();
+            lastShootTime = Time.time; // 마지막 발사 시간 업데이트
+        }
+
+        if (Input.GetKey(KeyCode.N) && Time.time >= lastShootTime + shootCooldown)  // N키 샷건
+        {
+            ShootGun();
+            lastShootTime = Time.time; // 마지막 발사 시간 업데이트
+        }
+
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            ToggleBazookaAutoFire();
+        }
     }
 
     void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            rb.MovePosition(transform.position + new Vector3(0.0f, 1.0f, 0.0f) * Time.deltaTime * playerStat.getPlayerSpeed());
-        }
-
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            rb.MovePosition(transform.position - new Vector3(0.0f, 1.0f, 0.0f) * Time.deltaTime * playerStat.getPlayerSpeed());
-        }
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            rb.MovePosition(transform.position - new Vector3(1.0f, 0.0f, 0.0f) * Time.deltaTime * playerStat.getPlayerSpeed());
-        }
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            rb.MovePosition(transform.position + new Vector3(1.0f, 0.0f, 0.0f) * Time.deltaTime * playerStat.getPlayerSpeed());
-        }
+        Vector2 nextVec = inputVec.normalized * playerStat.getPlayerSpeed() * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + nextVec);
 
         if (HPBar != null)
         {
@@ -95,29 +124,7 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    void ShootBullet()
-    {
-        GameObject nearestMonster = FindNearestMonster();
-        if (nearestMonster != null)
-        {
-            Vector3 direction = (nearestMonster.transform.position - transform.position).normalized;
-            Vector3 bulletSpawnPosition = transform.position + direction * 0.5f; // 플레이어 위치에서 약간 떨어진 위치
-            GameObject bullet = Instantiate(Bullet, bulletSpawnPosition, Quaternion.identity);
-            Rigidbody2D bulletRb = bullet.GetComponent<Rigidbody2D>();
-            if (bulletRb != null)
-            {
-                bulletRb.velocity = direction * BulletSpeed;
-            }
-
-            // 플레이어와 총알 간의 충돌을 무시
-            Collider2D playerCollider = GetComponent<Collider2D>();
-            Collider2D bulletCollider = bullet.GetComponent<Collider2D>();
-            if (playerCollider != null && bulletCollider != null)
-            {
-                Physics2D.IgnoreCollision(playerCollider, bulletCollider);
-            }
-        }
-    }
+    
 
     void MeleeAttack()
     {
@@ -176,5 +183,134 @@ public class PlayerController : MonoBehaviour
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position + new Vector3(-1.0f, 0.0f, 0.0f), 0.5f); // 왼쪽 공격 범위 표시
         Gizmos.DrawWireSphere(transform.position + new Vector3(1.0f, 0.0f, 0.0f), 0.5f); // 오른쪽 공격 범위 표시
+    }
+
+    void ShootGun() // 샷건
+    {
+        Vector3 baseDirection = (inputVec == Vector2.zero) ? new Vector3(lastDirection.x, lastDirection.y, 0) : new Vector3(inputVec.x, inputVec.y, 0).normalized;  // 기본 발사 방향을 inputVec를 사용하여 설정
+        if (baseDirection == Vector3.zero)  // inputVec이 0일 때 총알을 발사하지 않음
+            return;
+
+        // 각도를 기준으로 3발의 총알을 발사
+        for (int i = -1; i <= 1; i++)
+        {
+            // i값에 따라 각도를 조정합니다. -1, 0, 1에 따라 -30도, 0도, 30도
+            float angle = 30.0f * i;
+            Vector3 direction = Quaternion.Euler(0, 0, angle) * baseDirection;
+
+            // 총알 생성 위치를 플레이어 앞쪽으로 설정
+            Vector3 bulletSpawnPosition = transform.position + direction * 0.5f; // 플레이어 위치에서 약간 떨어진 위치
+
+            // 총알을 생성하고 발사
+            GameObject bullet = Instantiate(Bullet, bulletSpawnPosition, Quaternion.identity);
+            Rigidbody2D bulletRb = bullet.GetComponent<Rigidbody2D>();
+            if (bulletRb != null)
+            {
+                bulletRb.velocity = direction * BulletSpeed;
+            }
+            // 총알의 Sprite Renderer 설정
+            SpriteRenderer bulletSr = bullet.GetComponent<SpriteRenderer>();
+            if (bulletSr != null)
+            {
+                bulletSr.sortingLayerName = "Player"; // 총알의 sorting layer를 플레이어와 같은 것으로 설정
+                bulletSr.sortingOrder = 1; // 플레이어보다 앞에 보이도록 설정
+            }
+            // 플레이어와 총알 간의 충돌을 무시
+            Collider2D playerCollider = GetComponent<Collider2D>();
+            Collider2D bulletCollider = bullet.GetComponent<Collider2D>();
+            if (playerCollider != null && bulletCollider != null)
+            {
+                Physics2D.IgnoreCollision(playerCollider, bulletCollider);
+            }
+
+            // 총알의 canPenetrate 변수를 true로 설정하여 관통 가능 설정
+            Bullet bulletScript = bullet.GetComponent<Bullet>();
+            if (bulletScript != null)
+            {
+                bulletScript.canPenetrate = true;
+            }
+        }
+    }
+
+
+    void ShootBullet()
+    {
+        GameObject nearestMonster = FindNearestMonster();
+        if (nearestMonster != null)
+        {
+            Vector3 direction = (nearestMonster.transform.position - transform.position).normalized; 
+            Vector3 bulletSpawnPosition = transform.position + direction * 0.5f; // 플레이어 위치에서 약간 떨어진 위치
+            GameObject bullet = Instantiate(Bullet, bulletSpawnPosition, Quaternion.identity);
+            Rigidbody2D bulletRb = bullet.GetComponent<Rigidbody2D>();
+            if (bulletRb != null)
+            {
+                bulletRb.velocity = direction * BulletSpeed;
+            }
+            // 총알의 Sprite Renderer 설정
+            SpriteRenderer bulletSr = bullet.GetComponent<SpriteRenderer>();
+            if (bulletSr != null)
+            {
+                bulletSr.sortingLayerName = "Player"; // 총알의 sorting layer를 플레이어와 같은 것으로 설정
+                bulletSr.sortingOrder = 1; // 플레이어보다 앞에 보이도록 설정
+            }
+            // 플레이어와 총알 간의 충돌을 무시
+            Collider2D playerCollider = GetComponent<Collider2D>();
+            Collider2D bulletCollider = bullet.GetComponent<Collider2D>();
+            if (playerCollider != null && bulletCollider != null)
+            {
+                Physics2D.IgnoreCollision(playerCollider, bulletCollider);
+            }
+        }
+    }
+    void ShootBazooka()
+    {
+        GameObject nearestMonster = FindNearestMonster();
+        if (nearestMonster != null)
+        {
+            Vector3 direction = (nearestMonster.transform.position - transform.position).normalized;
+            Vector3 bulletSpawnPosition = transform.position + direction * 0.5f; // 플레이어 위치에서 약간 떨어진 위치
+            GameObject bullet = Instantiate(Bullet, bulletSpawnPosition, Quaternion.identity);
+            Rigidbody2D bulletRb = bullet.GetComponent<Rigidbody2D>();
+            if (bulletRb != null)
+            {
+                bulletRb.velocity = direction * BulletSpeed;
+            }
+            // 총알의 Sprite Renderer 설정
+            SpriteRenderer bulletSr = bullet.GetComponent<SpriteRenderer>();
+            if (bulletSr != null)
+            {
+                bulletSr.sortingLayerName = "Player"; // 총알의 sorting layer를 플레이어와 같은 것으로 설정
+                bulletSr.sortingOrder = 1; // 플레이어보다 앞에 보이도록 설정
+            }
+            // 플레이어와 총알 간의 충돌을 무시
+            Collider2D playerCollider = GetComponent<Collider2D>();
+            Collider2D bulletCollider = bullet.GetComponent<Collider2D>();
+            if (playerCollider != null && bulletCollider != null)
+            {
+                Physics2D.IgnoreCollision(playerCollider, bulletCollider);
+            }
+
+            // 총알의 isBazukapo 변수를 true로 설정하여 주변적에게 피해를 입힘
+            Bullet exploseScript = bullet.GetComponent<Bullet>();
+            if (exploseScript != null)
+            {
+                exploseScript.isBazukapo = true;
+            }
+
+        }
+    }
+
+    // 바주카포 발사를 ON OFF로 구현
+    void ToggleBazookaAutoFire()
+    {
+        isBazookaAutoFire = !isBazookaAutoFire;
+        if (isBazookaAutoFire)
+        {
+            InvokeRepeating("ShootBazooka", 0f, bazookaCooldown);
+        }
+        else
+        {
+            CancelInvoke("ShootBazooka");
+        }
     }
 }
