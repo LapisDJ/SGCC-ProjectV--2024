110e752131cb668515aee114f1376795217b108c
moxnoxox <139220034+moxnoxox@users.noreply.github.com>
2024-08-21 01:37:30 +0900

    refactor : 패시브 스킬 스크립트 일부 수정
    잘못 달린 주석 수정
    미처 커밋하지 못한 스크립트 추가

--- Assets/Scripts/Controllers/Skills/Skills/PassiveSkill/LearnPill.cs
+++ Assets/Scripts/Controllers/Skills/Skills/PassiveSkill/LearnPill.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class LearnPill : PassiveSkill
 {
     public LearnPill() : base("LearnPill", 0.3f, 0f) {}// 생성자 : 스킬명, 1랩 데미지, 1랩 쿨타임
-    public override void LevelUp() // 연산 보조 장치 레벨업 로직
+    public override void LevelUp() // 학습 증진 알약 레벨업 로직
     {
         base.LevelUp(); // 스킬 레벨업
         this.effect += 0.1f; // 1레벨을 제외한 모든 레벨에서 효과가 10%p씩 상승.
