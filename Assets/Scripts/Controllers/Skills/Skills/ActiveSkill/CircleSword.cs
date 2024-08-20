04d7df1d7f8717d8bf91726c5bc22134da087d09
LapisDJ <137609732+LapisDJ@users.noreply.github.com>
2024-08-21 03:11:59 +0900

    feat: 칼날 의수 데미지 로직 구현 완료

--- Assets/Scripts/Controllers/Skills/Skills/ActiveSkill/CircleSword.cs
+++ Assets/Scripts/Controllers/Skills/Skills/ActiveSkill/CircleSword.cs
@@ -7,9 +7,34 @@ public class CircleSword : Skill
     public CircleSword() : base("CircleSword", 2f, 4f) { } // 생성자 : 스킬명, 1랩 데미지, 1랩 쿨타임
 
     private float circleAttackRadius = 2f; // 원 범위 반지름
+    WeaknessType weaknessType = WeaknessType.Slash;
     public override void Activate(GameObject target) // 몬스터와 상호 작용 로직
     {
+        Monster monster;
+        float totalDamage = 0f; // 몬스터가 입는 총 데미지
 
+        // 범위 내의 모든 콜라이더를 가져옴 (히트스캔 방식)
+        Collider2D[] hitColliders = Physics2D.OverlapCircleAll(player.transform.position, circleAttackRadius);
+
+        foreach (var hitCollider in hitColliders) // 범위 안의 모든 몬스터에 대하여 반복문
+        {
+            if (hitCollider.CompareTag("Monster1") || hitCollider.CompareTag("Monster3") || hitCollider.CompareTag("Monster2"))
+            {
+                monster = hitCollider.GetComponent<Monster>();
+                float weaknessMultipler = (hitCollider.CompareTag("Monster1") || hitCollider.CompareTag("Monster3")) ? 1.5f : 1f;
+
+                DamageInfo damageInfo = new DamageInfo
+                {
+                    skillDamage = this.skillDamage,
+                    playerDamage = player.playerStat.attackDamageByLevel,
+                    weaknessMultipler = weaknessMultipler,
+                    isCritical = player.playerStat.CheckCritical()
+                };
+
+                totalDamage = finalDamage(damageInfo);
+                monster.TakeDamage(totalDamage);
+            }
+        }
     }
 
     public override void LevelUp() // 칼날 의수(원형 참격) 레벨업 로직
@@ -17,10 +42,10 @@ public class CircleSword : Skill
         base.LevelUp(); // 스킬 레벨업
         this.skillDamage += 1f;
 
-        switch(level)
+        switch (level)
         {
             case 3: // 2->3랩: 쿨타임 1초 감소
-                this.cooldown--; 
+                this.cooldown--;
                 break;
             case 5: // 3->4랩: 반지름 2->2.25
                 this.circleAttackRadius = 2.25f;
