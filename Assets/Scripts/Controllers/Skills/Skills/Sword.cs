5f792cdeeeb381cf168c143e5d0aa2828b0adfb9
LapisDJ <137609732+LapisDJ@users.noreply.github.com>
2024-08-20 22:09:49 +0900

    feat: 컨플릭트 해결중

--- Assets/Scripts/Controllers/Skills/Skills/Sword.cs
+++ Assets/Scripts/Controllers/Skills/Skills/Sword.cs
@@ -7,7 +7,11 @@ public class Sword : MonoBehaviour
 
     void Start()
     {
+<<<<<<< HEAD
         
+=======
+    
+>>>>>>> parent of 5db4d01 (Merge branch 'BaseSetting')
     }
 
     void Update()
