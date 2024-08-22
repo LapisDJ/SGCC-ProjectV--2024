926d6046326cd95fe2c52b4da7034547d25609f2
moxnoxox <139220034+moxnoxox@users.noreply.github.com>
2024-08-23 00:24:02 +0900

    Feat : 정산 씬 버튼 스크립트 연결, 메인 메뉴 버튼 수정.

--- Assets/Scripts/UI/Button/MainButton.cs
+++ Assets/Scripts/UI/Button/MainButton.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class MainButton : MonoBehaviour
 {
+    //main scene
     public void OnClickNewgame()
     {
         LoadingSceneController.Loadscene("Character choose");
@@ -20,5 +21,45 @@ public class MainButton : MonoBehaviour
     public void OnClickQuit()
     {
         Application.Quit();
+    }
+    //character choose scene
+    int characternum;
+    public void FirstCharacterButton()
+    {
+        characternum = 1;
+    }
+    public void Confirmbutton()
+    {
+        SceneManager.LoadScene("Prototype");
+    }
+    public void Previousbutton()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+    //finish scene
+    [SerializeField] GameObject recheckwindow;
+    void Start()
+    {
+        recheckwindow.SetActive(false);
+    }
+    public void GotoMainmenuButton()
+    {
+        recheckwindow.SetActive(true);
+    }
+    public void Recheckyes()
+    {
+        LoadingSceneController.Loadscene("Main Menu");
+    }
+    public void Recheckno()
+    {
+        recheckwindow.SetActive(false);
+    }
+    public void Retry()
+    {
+        LoadingSceneController.Loadscene("Prototype");
+    }
+    public void Nextchapter()
+    {
+        LoadingSceneController.Loadscene("Prototype");
     }
 }
