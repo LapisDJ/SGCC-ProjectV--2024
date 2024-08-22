098e00124590b764fc8534ae104089ac7d6dba16
moxnoxox <139220034+moxnoxox@users.noreply.github.com>
2024-08-23 03:42:15 +0900

    Feat : 레벨업 UI와 로직 연결(디버깅 로그창에 뜨긴해요)

--- Assets/Scripts/Controllers/Skills/SkillManager.cs
+++ Assets/Scripts/Controllers/Skills/SkillManager.cs
@@ -8,7 +8,6 @@ public class SkillManager : MonoBehaviour
 {
     public int maxActiveSkills = 4; //최대 액티브 개수
     public int maxPassiveSkills = 6; //최대 패시브 개수
-    public static bool trigger = false;
 
     private List<Skill> availableActiveSkills = new List<Skill>(); // 모든 액티브 스킬 리스트
     private List<PassiveSkill> availablePassiveSkills = new List<PassiveSkill>(); // 모든 패시브 스킬 리스트
@@ -50,11 +49,7 @@ public class SkillManager : MonoBehaviour
     }
     void Update()
     {
-        if(trigger)
-        {
-            SkillLevelUP();
-            trigger = false;
-        }
+
     }
 
     private List<Skill> GetRandomSkillChoices() // 로직에 따라 액티브 스킬 2개 반환
@@ -68,8 +63,8 @@ public class SkillManager : MonoBehaviour
             {
                 rand2 = UnityEngine.Random.Range(0,maxActiveSkills);
             }
-            retList.Add(activeSkills[rand1]);
-            retList.Add(activeSkills[rand2]);
+            retList.Add(availableActiveSkills[rand1]);
+            retList.Add(availableActiveSkills[rand2]);
         }
         else//액티브 스킬 가짓수를 모두 채우지 않았으면 모든 리스트에서 가져옴
         {
@@ -96,14 +91,14 @@ public class SkillManager : MonoBehaviour
             {
                 rand2 = UnityEngine.Random.Range(0,maxPassiveSkills);
             }
-            retList.Add(passiveSkills[rand1]);
-            retList.Add(passiveSkills[rand2]);
+            retList.Add(availablePassiveSkills[rand1]);
+            retList.Add(availablePassiveSkills[rand2]);
         }
         else
         {
             int rand1 = UnityEngine.Random.Range(0,9);
             int rand2 = UnityEngine.Random.Range(0,9);
-            while(rand1 != rand2)
+            while(rand1 == rand2)
             {
                 rand2 = UnityEngine.Random.Range(0,9);
             }
@@ -116,28 +111,75 @@ public class SkillManager : MonoBehaviour
     public static void SkillLevelUP()
     {
         //0레벨이면 리스트에 등록, 1레벨 이상이면 레벨업만
-        if(skillchoice < 2)
+        if (skillchoice < 2)
         {
-            if (skillchoices[skillchoice].level == 0)
-            {
-                activeSkills.Add(skillchoices[skillchoice]);
-                skillchoices[skillchoice].LevelUp();
-            }
-            else
-            {
-                skillchoices[skillchoice].LevelUp();
-            }
+            LevelUpSkill(skillchoices[skillchoice]);//고른 스킬이 액티브일때 고른 스킬과 활성화된 스킬 리스트를 매개로 하는 함수 호출
         }
         else
         {
-            if (passivechoices[skillchoice-2].level == 0)
+            LevelUpPassiveSkill(passivechoices[skillchoice - 2]);//고른 스킬이 패시브일때 고른 스킬과 활성화된 스킬 리스트를 매개로 하는 함수 호출
+        }
+        foreach (Skill active in activeSkills)
+        {
+            Debug.Log(active.skillName);//지금까지 나온 모든 액티브 스킬의 이름을 콘솔창에 띄움
+        }
+        foreach (PassiveSkill passive in passiveSkills)
+        {
+            Debug.Log(passive.skillName);//지금까지 나온 모든 패지브 스킬의 이름을 콘솔창에 띄움
+        }
+        skillchoices.Clear();//랜덤으로 두 개씩 뽑은 스킬 리스트들 초기화.
+        passivechoices.Clear();
+    }
+    static bool cantfind = true;//지금까지 고른 적 없는 스킬일 때 true
+    private static void LevelUpSkill(Skill skill)
+    {
+        cantfind = true;//일단 true로 초기화
+        foreach(Skill active in activeSkills)
+        {
+            if(active == skill)//지금까지 고른 스킬리스트에 레벨업할 스킬이 있다면 실행.
             {
-                passiveSkills.Add(passivechoices[skillchoice-2]);
-                passivechoices[skillchoice-2].LevelUp();
+                active.LevelUp();//스킬 레벨업(고른 적 있는 리스트의)
+                cantfind = false;//고른적 있다고 바꿔버림.
+                break;
             }
-            else
+        }
+        if(cantfind)//이전 if문이 발동되지 않음(리스트에 없음)
+        {
+            activeSkills.Add(skill);//리스트에 없다는 이야기이므로 일단 레벨업
+            foreach(Skill active in activeSkills)//아까 Add한 스킬 찾아서 레벨업.
             {
-                passivechoices[skillchoice-2].LevelUp();
+                if(active == skill)
+                {
+                    active.LevelUp();
+                    cantfind = false;
+                    break;
+                }
+            }
+        }
+    }
+    private static void LevelUpPassiveSkill(PassiveSkill skill)
+    {
+        cantfind = true;
+        foreach(PassiveSkill passive in passiveSkills)
+        {
+            if(passive == skill)
+            {
+                passive.LevelUp();
+                cantfind = false;
+                break;
+            }
+        }
+        if(cantfind)
+        {
+            passiveSkills.Add(skill);
+            foreach(PassiveSkill passive in passiveSkills)
+            {
+                if(passive == skill)
+                {
+                    passive.LevelUp();
+                    cantfind = false;
+                    break;
+                }
             }
         }
     }
--- Assets/Scripts/UI/UI_Manager.cs
+++ Assets/Scripts/UI/UI_Manager.cs
@@ -26,6 +26,7 @@ public class UI_Manager : MonoBehaviour
         MissionUI.SetActive(false);
         ExitSceneUI.SetActive(false);
         pausemenu.SetActive(false);
+        isskillchoose = false;
         ispause = false;
     }
     public void SkillChooseStart()//진행중인 게임을 일시정지하고 레벨업할 스킬을 선택하는 창으로 진입.
@@ -34,31 +35,30 @@ public class UI_Manager : MonoBehaviour
         skillmanager.LevelUp();
         SkillChoose.SetActive(true);
     }
-    public int tempskillchoice;
+    public int tempskillchoice = 9;
     //몇번째 버튼인지에 따라 임시값을 설정하고 확인버튼을 눌렀을 때 skillchoice변수를 변형한다.
-    public void firstskillchooosebutton()
+    public void Firstskillchooosebutton()
     {
         tempskillchoice = 0;
     }
-    public void secondskillchooosebutton()
+    public void Secondskillchooosebutton()
     {
         tempskillchoice = 1;
     }
-    public void thirdskillchooosebutton()
+    public void Thirdskillchooosebutton()
     {
         tempskillchoice = 2;
     }
-    public void fourthskillchooosebutton()
+    public void Fourthskillchooosebutton()
     {
         tempskillchoice = 3;
     }
-    public void confirmskillchoosebutton()
+    public void Confirmskillchoosebutton()
     {
         SkillManager.skillchoice = tempskillchoice;
         SkillChoose.SetActive(false);
         Time.timeScale = 1.0f;
-        isskillchoose = false;
-        SkillManager.trigger = true;
+        SkillManager.SkillLevelUP();
     }
     void FixedUpdate()
     {
