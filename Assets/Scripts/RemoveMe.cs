23e2dfd512045984d12b807b623598e14a492a93
LapisDJ <137609732+LapisDJ@users.noreply.github.com>
2024-08-28 12:22:34 +0900

    feat: RemoveMe 삭제하십시오

---
+++ Assets/Scripts/RemoveMe.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveMe : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
